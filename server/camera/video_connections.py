2e39faeacd71e3876bd44844ecb6ca2efc5d6a42
Zsolt Palmai <zpalmai@gmail.com>
2019-05-26 22:55:58 +0200

    Add ip address fetching

--- server/camera/video_connections.py
+++ server/camera/video_connections.py
@@ -1,10 +1,11 @@
 import socket
 from .camera import CameraFeed
 
+
 def start_video_connection():
     server_socket = socket.socket()
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    host = '192.168.0.31'
+    host = get_ip_address()
     port = 1235
     server_socket.bind((host, port))
 
@@ -13,6 +14,17 @@ def start_video_connection():
     print("Connected to - ", address, "\n")
 
     CameraFeed(conn).send_images()
+
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+    except Exception as e:
+        print("Couldn't get IP address: ", e)
+    finally:
+        s.close()
 
 
 
